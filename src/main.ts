38ea2275e6643554776f9254b887067e14631d09
ElDarwon123 <dsgc0614@gmail.com>
2024-06-06 17:35:02 -0500

    Feature: Swagger implement

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const conf = new DocumentBuilder()
+    .setTitle('AgroEmprende')
+    .setDescription('its a formative project')
+    .setVersion('1.0.0')
+    .addTag('project')
+    .build();
+  const doc = SwaggerModule.createDocument(app, conf)
+  SwaggerModule.setup('api', app, doc)
   // configuración para env
   app.useGlobalPipes(new ValidationPipe());
   const config = app.get(ConfigService);
