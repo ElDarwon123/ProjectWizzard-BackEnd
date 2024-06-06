532e7a1bd197dadd42e18c359a24e156f68a4de8
ElDarwon123 <dsgc0614@gmail.com>
2024-06-06 12:29:32 -0500

    Revert "aaaa"
    This reverts commit 97b95756bb1322a2ff22ec4f1a956ccef843fc17.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import {
   Body,
   Controller,
@@ -12,7 +13,7 @@ import {
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { AuthGuard } from './auth.guard';
-// autenticationn
+// autenticatio
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
