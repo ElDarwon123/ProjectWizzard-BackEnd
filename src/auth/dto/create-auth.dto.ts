6791d42b397225b2a9c3198b05368a95d4893d7c
ElDarwon123 <dsgc0614@gmail.com>
2024-06-06 17:56:23 -0500

    Feature: Swagger Schema update

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,4 +1,5 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty, ApiBody } from '@nestjs/swagger';
 
 export class CreateAuthDto {
   @IsString()
--- src/auth/dto/update-auth.dto.ts
+++ src/auth/dto/update-auth.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAuthDto } from './create-auth.dto';
+import { ApiProperty, ApiBody } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateAuthDto extends PartialType(CreateAuthDto) {}
+export class UpdateAuthDto extends PartialType(CreateAuthDto) {
+    @IsString()
+    @IsNotEmpty()
+    email: string;
+    @IsString()
+    @IsNotEmpty()
+    contrasena: string;
+}
