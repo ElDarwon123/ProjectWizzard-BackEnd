c2a671ef0d94b82aae6d4dfb887f7f826e98feee
Kevin-nn <thekevincaldon@gmail.com>
2024-06-27 17:32:52 -0500

    n

--- src/proyecto/proyecto.controller.ts
+++ src/proyecto/proyecto.controller.ts
@@ -5,35 +5,39 @@ import { CreateProyectoDto } from './dtos/create.proyecto.dto';
 import { UpdateProyectoDto } from './dtos/update-proyecto.dto';
 import { Proyecto } from './schema/proyecto.shema';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
-import { RolesGuard } from 'src/auth/guards/role.guard';
 import { Roles } from 'src/auth/decorators/roles.decorator';
+import { RolesGuard } from 'src/auth/guards/role.guard';
 import { RolesEnum } from 'src/enums/role.enum';
 
 @Controller('proyectos')
-@UseGuards(AuthGuard, RolesGuard)
-@Roles(RolesEnum.Aprendiz) //a nivel de clase
-
-
 export class ProyectoController {
   constructor(private readonly proyectoService: ProyectoService) {}
 
   @Post()
+  @Roles(RolesEnum.Aprendiz, RolesEnum.Admin)
+  @UseGuards(AuthGuard, RolesGuard)
   @UsePipes(new ValidationPipe())
   create(@Body() createProyectoDto: CreateProyectoDto): Promise<Proyecto> {
     return this.proyectoService.create(createProyectoDto);
   }
 
   @Get()
+  @Roles(RolesEnum.Aprendiz, RolesEnum.Admin)
+  @UseGuards(AuthGuard, RolesGuard)
   findAll(): Promise<Proyecto[]> {
     return this.proyectoService.findAll();
   }
 
   @Get(':id')
+  @Roles(RolesEnum.Aprendiz, RolesEnum.Admin)
+  @UseGuards(AuthGuard, RolesGuard)
   findOne(@Param('id') id: string): Promise<Proyecto> {
     return this.proyectoService.findOne(id);
   }
 
   @Patch(':id')
+  @Roles(RolesEnum.Aprendiz, RolesEnum.Admin)
+  @UseGuards(AuthGuard, RolesGuard)
   @UsePipes(new ValidationPipe())
   update(
     @Param('id') id: string,
@@ -43,6 +47,8 @@ export class ProyectoController {
   }
 
   @Delete(':id')
+  @Roles(RolesEnum.Aprendiz, RolesEnum.Admin)
+  @UseGuards(AuthGuard, RolesGuard)
   remove(@Param('id') id: string): Promise<Proyecto> {
     return this.proyectoService.remove(id);
   }
